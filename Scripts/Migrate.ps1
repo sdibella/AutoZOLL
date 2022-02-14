160a0911f68784c70fe7d3b8013ddc6390aeb66e
sdibella <sdibella@me.com>
2022-02-14 09:22:31 -0700

    beta

--- Scripts/Migrate.ps1
+++ Scripts/Migrate.ps1
@@ -1,13 +1,11 @@
-# Set $envieronment variables and run script. You're welcome.
-
-$RNePCRUser = "yes" # yes/no
+# Set $environment variables and run script. You're welcome.
 
 $environment = @{
     servers = @{ 
         Live = @('.')
         Target = @{ 
-            RNDB = '.'
-            Svcs = '.'
+            RNDB = ''
+            Svcs = ''
             Nav = '.'
             ePCR = '.'
         }
@@ -21,31 +19,31 @@ $environment = @{
 }
 
 # build ZOLL database hash table
- $dbList = @{
+$dbList = @{
     RNDB = @('RCSQL', 'Forms', 'PPAPI')
-    Services = @('RNEventing', 'RNEnterprise', 'UDXMapping')
-    Navigator = @('ZollData.NavigatorServer')
+    Svcs = @('RNEventing', 'RNEnterprise', 'UDXMapping')
+    Nav = @('ZollData.NavigatorServer')
     ePCR = @('eDistribution', 'Extract', 'PCRServer', 'ZollData.MSCS.PCR.Archive', 'Zolldata.StaticData', 'Zolldata.System','ZOLLData.MercuryMessage')
- }
+}
 
- # Create ePCR config list or remove ePCR databases
- if ( $RNePCRUser = 'no' ) {
-    $dbList.Remove('ePCR')
- }
-
- # Begin migration
+# Begin migration
 
 # Start robocopy as a background job
 Start-Job -ScriptBlock { robocopy $args[0] $args[1] /mt /z /e /copyall } -ArgumentList $environment['filePaths']['currentCentralShare'], $environment['filePaths']['newCentralShare']
 
- # Backup and Restore databases
- foreach ( $server in $environment['servers']['Live'] ) {
+# Backup and Restore databases
+foreach ( $server in $environment['servers']['Live'] ) {
     $dbsPresent = [System.Collections.ArrayList](Get-SqlDatabase -ServerInstance $server) | Where-Object { (-not($_ -match 'master|model|msdb|tempdb')) } | Select-Object -expand Name
     foreach ( $db in $dbsPresent ) {
         $backupFile = $environment['filePaths']['backupLocation'] + "\$db.bak"
         $targetGroup = $dbList.keys | Where-Object { $dbList[$_] -eq $db }
-        Backup-SqlDatabase -ServerInstance $server -Database $db -CopyOnly -BackupFile $backupFile
-        Write-Output "$db backup successfull, attempting to restore to target server as background job"
-        Restore-SqlDatabase -ReplaceDatabase -ServerInstance $environment['servers']['Target'][$targetGroup] -Database $db -BackupFile $backupFile
+        Backup-SqlDatabase -CopyOnly -ServerInstance $server -Database $db -BackupFile $backupFile
+        Write-Output "$db backup successfull"
+        Restore-SqlDatabase -RestoreAction Files -ReplaceDatabase -ServerInstance $environment['servers']['Target'][$targetGroup] -Database $db -BackupFile $backupFile
+        Write-Output "$db restored succesfully"
+        # Start-Job -ScriptBlock { Restore-SqlDatabase -RestoreAction Files -ReplaceDatabase -ServerInstance $args[0] -Database $args[1] -BackupFile $args[2] } -ArgumentList $environment['servers']['Target'][$targetGroup], $db, $backupFile
     }
- }
+}
+Get-Job | Wait-Job
+Write-Output "Fin."
+Read-Host -Prompt "Press Enter to exit"
